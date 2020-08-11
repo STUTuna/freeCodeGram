d5c30af9acd8cf68f1c83c325b09051823e1f98b
Tuna <s15113114@stu.edu.tw>
2020-08-11 22:58:09 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_ohc=LFHchPCFeR0AX_A7cye&oh=6007370dbf3454e6cf6307be2391c0a0&oe=5F56A067" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5 pl-5">
+            <div><h1>Tuna's Instagram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong>posts</div>
+                <div class="pr-5"><strong>23k</strong>follows</div>
+                <div class="pr-5"><strong>212</strong>following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Tuna's Site</div>
+            <div>一些自我介紹的文字</div>
+            <div><a href="#">Tuna's Site</a></div>
+
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/e35/c95.0.559.559a/96936602_574334026620306_7948073701358189323_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=0FSRfdOUFwoAX8lpfhc&oh=7d142a8746e94d0ab4fbf512ce6288bc&oe=5F559BA2">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fkhh1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c2.0.745.745a/s640x640/73423634_2423788067939118_598762339110485792_n.jpg?_nc_ht=instagram.fkhh1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=UI7k9_O3kKcAX_Immal&oh=7e401c29be66a2cec6e7fab2ee15a649&oe=5F579DF0">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fkhh1-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.0.749.749a/s640x640/62405980_352431338752162_8074657814463950764_n.jpg?_nc_ht=instagram.fkhh1-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=QO_rWDn9AbEAX_sdB0I&oh=c060e3a8de100b23b5760e432ceb9260&oe=5F55946D">
+        </div>
+
+    </div>
+
+</div>
+@endsection
